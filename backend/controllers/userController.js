11a4e8e1923b636318664827824ce05df194cadb
mausam30 <mausam.borah30@gmail.com>
2024-04-07 18:07:41 +0530

    Logout added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,22 +39,22 @@ const createUser = asyncHandler(async (req, res) => {
 
 });
 
-const loginUser = asyncHandler(async(req,res)=>{
-    const {email,password} = req.body
-    
-    const existingUser = await User.findOne({email})
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body
 
-    if(existingUser){
+    const existingUser = await User.findOne({ email })
+
+    if (existingUser) {
         const isPasswordValid = await bcrypt.compare(password, existingUser.password)
 
-        if(isPasswordValid){
-            createToken(res,existingUser._id)
+        if (isPasswordValid) {
+            createToken(res, existingUser._id)
 
             res.status(201).json({
-                _id:existingUser._id,
-                username:existingUser.username,
-                email:existingUser.email,
-                isAdmin:existingUser.isAdmin,
+                _id: existingUser._id,
+                username: existingUser.username,
+                email: existingUser.email,
+                isAdmin: existingUser.isAdmin,
             });
             return;
         }
@@ -62,9 +62,14 @@ const loginUser = asyncHandler(async(req,res)=>{
 })
 
 
-const logoutCurrentUser = asyncHandler(async(req,res)=>{
-    res.send("Logout");
-})
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+    res.cookie('jwt','',{
+        httpOnly: true,
+        expires: new Date(0),
+    })
+
+    res.status(200).json({message: "Logged out successfully"})
+});
 
 
-export { createUser, loginUser };
+export { createUser, loginUser, logoutCurrentUser };
